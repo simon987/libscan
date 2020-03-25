a24d4dc53859d0dd4ad9dbc2f5ef9b2f14f84c6e
simon987 <me@simon987.net>
2020-03-25 09:44:34 -0400

    text component

---
+++ src/macros.h
@@ -0,0 +1,13 @@
+#ifndef	FALSE
+#define	FALSE	(0)
+#endif
+
+#ifndef	TRUE
+#define	TRUE	(!FALSE)
+#endif
+
+#undef	MAX
+#define MAX(a, b)  (((a) > (b)) ? (a) : (b))
+
+#undef	MIN
+#define MIN(a, b)  (((a) < (b)) ? (a) : (b))
